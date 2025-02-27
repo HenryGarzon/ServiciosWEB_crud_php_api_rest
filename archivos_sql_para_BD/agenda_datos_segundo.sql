585c0df934f425511faf5a92d157a027b8b22441
HenryAGS <hagarzons@gmail.com>
2025-02-27 00:46:58 -0500

    Script de creación de la base de datos

---
+++ archivos_sql_para_BD/agenda_datos_segundo.sql
@@ -0,0 +1,10 @@
+
+TRUNCATE TABLE `tareas`;
+INSERT INTO `tareas` (`id`, `titulo`, `descripcion`, `prioridad`) VALUES
+	(1, 'Maestria', 'Persistencia', 1),
+	(2, 'Comprar elementos de senderismo', 'Ingresa a plataforma e-commerce', 1),
+	(3, 'Hacer ejercicio', 'Correr 5km en la mañana', 2),
+	(4, 'Lavar el auto', 'Lavar y aspirar el auto los domingos', 2),
+	(5, 'Comprar frutas', 'Ir al mercado y comprar frutas frescas', 2),
+	(6, 'Lavar la motociceta', 'Lavar y aspirar la motociceta los viernes', 3),
+	(7, 'Lavar la motociceta', 'Lavar y aspirar la motociceta los viernes', 3);
